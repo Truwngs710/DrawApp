4c0b012fb13d750ce44d4d1d679d90570d6ee09d
Thuongnn1 <thuongnn@minori.com.vn>
2023-03-09 15:24:59 +0700

    save

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { SketchCanvas, SketchCanvasRef } from "rn-perfect-sketch-canvas";
+
+export default function App() {
+  const canvasRef = useRef<SketchCanvasRef>(null);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SketchCanvas
+        ref={canvasRef}
+        strokeColor={"black"}
+        strokeWidth={8}
+        containerStyle={styles.container}
+      />
+      <Button onPress={canvasRef.current?.reset} title="Reset" />
+      <Button
+        onPress={() => {
+          console.log(canvasRef.current?.toPoints());
+        }}
+        title="log poin"
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
